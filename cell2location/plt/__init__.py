ca1dfcdab12d9b9d170a6547080abf55fe607f2a
Alexander Aivazidis <aa16@sanger.ac.uk>
2021-04-29 14:04:32 +0100

    added and adapted 1D plotting functions and WTA notebook

--- cell2location/plt/__init__.py
+++ cell2location/plt/__init__.py
@@ -2,9 +2,11 @@ from .plot_expected_vs_obs import plot_expected_vs_obs
 from .plot_factor_spatial import plot_categ_spatial
 from .plot_factor_spatial import plot_factor_spatial
 from .mapping_video import plot_spatial as plot_spatial
-from .plot_absolute_abundance_1D import plot_absolute_abundances_1D
+from .plot_in_1D import plot_absolute_abundances_1D
+from .plot_in_1D import plot_density_1D
 
 __all__ = [
+    "plot_density_1D",
     "plot_absolute_abundances_1D",
     "plot_expected_vs_obs",
     "plot_factor_spatial",
