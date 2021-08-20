894bc388d34cdfbd802cb77607e7e2304e93c2e6
Vitalii Kleshchevnikov <vk7@sanger.ac.uk>
2021-08-20 12:54:33 +0100

    added install_requires versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 
 def setup_package():
-    install_requires = ['pymc3', 'pyro-ppl', 'scvi-tools', 'torch', 'pygpu', 'numpy', 'pandas', 'scanpy']
+    install_requires = ['pymc3>=4.0', 'pyro-ppl>=1.7.0', 'scvi-tools>=0.12.2', 'torch>=1.9.0', 'pygpu', 'numpy', 'pandas', 'scanpy']
     metadata = dict(
         name='cell2location',
         version='0.05',
