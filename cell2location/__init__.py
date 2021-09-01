bd52ef734ec76f242a810ca354237cfb92015a79
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2021-09-01 22:17:15 +0100

    added softplus transform as default positive transform

--- cell2location/__init__.py
+++ cell2location/__init__.py
@@ -1,3 +1,7 @@
+from pyro.distributions import constraints
+from pyro.distributions.transforms import SoftplusTransform
+from torch.distributions import biject_to, transform_to
+
 from .run_c2l import run_cell2location
 from .run_colocation import run_colocation
 from .run_regression import run_regression
@@ -7,3 +11,9 @@ __all__ = [
     "run_regression",
     "run_colocation",
 ]
+
+
+@biject_to.register(constraints.positive)
+@transform_to.register(constraints.positive)
+def _transform_to_positive(constraint):
+    return SoftplusTransform()
