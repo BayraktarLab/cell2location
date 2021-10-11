3aa10072ef34675169f26e72da8767f77bb4cfad
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2021-10-11 01:39:17 +0100

    fixed isort

--- cell2location/models/_cell2location_model.py
+++ cell2location/models/_cell2location_model.py
@@ -9,10 +9,9 @@ import scvi
 from anndata import AnnData
 from pyro import clear_param_store
 from pyro.nn import PyroModule
-from scvi._docs import setup_anndata_dsp
 from scvi import _CONSTANTS
-from scvi.data._anndata import get_from_registry
-from scvi.data._anndata import _setup_anndata
+from scvi._docs import setup_anndata_dsp
+from scvi.data._anndata import _setup_anndata, get_from_registry
 from scvi.model.base import BaseModelClass, PyroSampleMixin, PyroSviTrainMixin
 
 from cell2location.models._cell2location_module import (
