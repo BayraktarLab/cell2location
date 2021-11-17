f37df252ce680faa14e32db5ab328ac200e8038e
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2021-11-17 15:42:46 +0000

    skip quantile test for messenger guides

--- tests/test_cell2location.py
+++ tests/test_cell2location.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pyro import poutine
 from scvi.data import synthetic_iid
 
 from cell2location import run_colocation
@@ -62,7 +63,8 @@ def test_cell2location():
     # minibatches of locations
     dataset = st_model.export_posterior(dataset, sample_kwargs={"num_samples": 10, "batch_size": 50})
     # test computing any quantile of the posterior distribution
-    st_model.posterior_quantile(q=0.5)
+    if isinstance(st_model.module.guide, poutine.messenger.Messenger):
+        st_model.posterior_quantile(q=0.5)
     # test computing expected expression per cell type
     st_model.module.model.compute_expected_per_cell_type(st_model.samples["post_sample_q05"], st_model.adata)
     ### test amortised inference with default cell2location model ###
