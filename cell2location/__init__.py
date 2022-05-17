0a8dcdbef07db81b8e1c2d10dcb609eefc56bd6d
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2022-05-17 22:51:57 +0100

    updated imports/notebook

--- cell2location/__init__.py
+++ cell2location/__init__.py
@@ -2,9 +2,11 @@ from pyro.distributions import constraints
 from pyro.distributions.transforms import SoftplusTransform
 from torch.distributions import biject_to, transform_to
 
+from . import models
 from .run_colocation import run_colocation
 
 __all__ = [
+    "models",
     "run_colocation",
 ]
 
