22648f3192dda8dc72dafb67b4642314e7d5b79e
Vitalii Kleshchevnikov <klv2706w@gmail.com>
2022-05-17 22:10:38 +0100

    removed pymc3-based model refs

--- cell2location/models/__init__.py
+++ cell2location/models/__init__.py
@@ -3,17 +3,11 @@ from ._cell2location_module import (
     LocationModelLinearDependentWMultiExperimentLocationBackgroundNormLevelGeneAlphaPyroModel,
 )
 from .downstream import CoLocatedGroupsSklearnNMF
-from .pymc3.LocationModelLinearDependentWMultiExperimentLocationBackgroundNormGeneAlpha import (
-    LocationModelLinearDependentWMultiExperimentLocationBackgroundNormGeneAlpha,
-)
-from .pymc3.LocationModelWTA import LocationModelWTA
 from .reference import RegressionModel
 
 __all__ = [
     "Cell2location",
     "RegressionModel",
-    "LocationModelLinearDependentWMultiExperimentLocationBackgroundNormGeneAlpha",
     "LocationModelLinearDependentWMultiExperimentLocationBackgroundNormLevelGeneAlphaPyroModel",
-    "LocationModelWTA",
     "CoLocatedGroupsSklearnNMF",
 ]
