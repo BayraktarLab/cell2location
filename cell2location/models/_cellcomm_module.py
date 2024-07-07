scaling by hierarchical annotations, correct unexpanded tiles, horseshoe prior, more likelihood options, zero diag & upper tri pathways, sqrt normalisation

cell2location/models/_cellcomm_module.py
@@ -26,7 +26,7 @@ class CellCommModule(PyroModule):
     Here, :math:`w_{s,f}` denotes regression weight of each reference signature :math:`f` at location :math:`s`, which can be interpreted as the expected number of cells at location :math:`s` that express reference signature :math:`f`;
     :math:`g_{f,g}` denotes the reference signatures of cell types :math:`f` of each gene :math:`g`, `cell_state_df` input ;
     """
-    n_pathways = 16
+    n_pathways = 150
     use_pathway_interaction_effect = True
     dropout_rate = 0.0
     use_non_negative_weights = False
@@ -60,6 +60,7 @@ class CellCommModule(PyroModule):
         use_cell_abundance_normalisation: bool = True,
         use_alpha_likelihood: bool = True,
         use_normal_likelihood: bool = False,
+        fixed_w_sf_mean_var_ratio: Optional[float] = None,
     ):
         super().__init__()
 
@@ -102,6 +103,7 @@ class CellCommModule(PyroModule):
         self.use_cell_abundance_normalisation = use_cell_abundance_normalisation
         self.use_alpha_likelihood = use_alpha_likelihood
         self.use_normal_likelihood = use_normal_likelihood
+        self.fixed_w_sf_mean_var_ratio = fixed_w_sf_mean_var_ratio
 
         self.weights = PyroModule()
 
@@ -167,6 +169,8 @@ class CellCommModule(PyroModule):
             kwargs["tiles_unexpanded"] = tensor_dict["tiles_unexpanded"]
         if "in_tissue" in tensor_dict.keys():
             kwargs["in_tissue"] = tensor_dict["in_tissue"].bool()
+        if "w_sf_lvl2" in tensor_dict.keys():
+            kwargs["w_sf_lvl2"] = tensor_dict["w_sf_lvl2"]
         return (signal_abundance, w_sf, ind_x, batch_index), kwargs
 
     def create_plates(
@@ -179,7 +183,12 @@ class CellCommModule(PyroModule):
         tiles_unexpanded: torch.Tensor = None,
         positions: torch.Tensor = None,
         in_tissue: torch.Tensor = None,
+        w_sf_lvl2: torch.Tensor = None,
     ):
+        if tiles_unexpanded is not None:
+            tiles_in_use = (tiles.mean(0) > torch.tensor(0.99, device=tiles.device)).bool()
+            obs_in_use = (tiles_unexpanded[:, tiles_in_use].sum(1) > torch.tensor(0.0, device=tiles.device)).bool()
+            idx = idx[obs_in_use]
         return pyro.plate("obs_plate", size=self.n_obs, dim=-2, subsample=idx)
 
     def list_obs_plate_vars(self):
@@ -290,15 +299,8 @@ class CellCommModule(PyroModule):
         tiles_unexpanded: torch.Tensor = None,
         positions: torch.Tensor = None,
         in_tissue: torch.Tensor = None,
+        w_sf_lvl2: torch.Tensor = None,
     ):
-        if tiles_unexpanded is not None:
-            tiles_in_use = tiles.sum(0).bool()
-            obs_in_use = tiles_unexpanded[:, tiles_in_use].sum(1).bool()
-            idx = idx[obs_in_use]
-            batch_index = batch_index[obs_in_use]
-            if positions is not None:
-                positions = positions[obs_in_use]
-        obs2sample = one_hot(batch_index, self.n_batch).float()
         obs_plate = self.create_plates(
             signal_abundance=signal_abundance,
             w_sf=w_sf,
@@ -309,6 +311,11 @@ class CellCommModule(PyroModule):
             positions=positions,
             in_tissue=in_tissue,
         )
+        if tiles_unexpanded is not None:
+            tiles_in_use = (tiles.mean(0) > torch.tensor(0.99, device=tiles.device)).bool()
+            obs_in_use = (tiles_unexpanded[:, tiles_in_use].sum(1) > torch.tensor(0.0, device=tiles.device)).bool()
+            batch_index = batch_index[obs_in_use]
+        obs2sample = one_hot(batch_index, self.n_batch).float()
 
         if getattr(self, "distances", None) is not None:
             distances = self.distances
@@ -332,31 +339,38 @@ class CellCommModule(PyroModule):
         )
         if not self.training and self.record_sr_occupancy:
             with obs_plate:
+                # {sr pair, location * cell type} -> {sr pair, location, cell type}
+                bound_receptor_abundance_src = rearrange(
+                    bound_receptor_abundance_src,
+                    "r (c f) -> r c f",
+                    f=self.receptor_abundance.shape[-1],
+                )
+                if tiles_unexpanded is not None:
+                    bound_receptor_abundance_src = bound_receptor_abundance_src[:, obs_in_use, :]
                 pyro.deterministic(
                     "bound_receptor_abundance_sr_c_f",
-                    # {sr pair, location * cell type} -> {sr pair, location, cell type}
-                    rearrange(
-                        bound_receptor_abundance_src,
-                        "r (c f) -> r c f",
-                        f=self.receptor_abundance.shape[-1],
-                    ),
+                    bound_receptor_abundance_src,
                 )
-        w_sf_mean_var_ratio_hyp = pyro.sample(
-            "w_sf_mean_var_ratio_hyp_lik",
-            dist.Gamma(self.w_sf_mean_var_ratio_tensor, self.ones).expand([1, 1]).to_event(2),
-        )  # prior mean 5.0
-        w_sf_mean_var_ratio = pyro.sample(
-            "w_sf_mean_var_ratio_lik",
-            dist.Exponential(w_sf_mean_var_ratio_hyp).expand([1, self.n_factors]).to_event(2),
-        )  # (self.n_batch, self.n_vars) prior mean 0.2
-        if self.use_normal_likelihood:
-            w_sf_mean_var_ratio = w_sf_mean_var_ratio + torch.tensor(1.0 / 50.0, device=w_sf_mean_var_ratio.device)
+        if self.fixed_w_sf_mean_var_ratio is not None:
+            w_sf_mean_var_ratio = torch.tensor(self.fixed_w_sf_mean_var_ratio, device=w_sf_mu_cell_comm.device)
         else:
-            w_sf_mean_var_ratio = self.ones / (
-                w_sf_mean_var_ratio + torch.tensor(1.0 / 50.0, device=w_sf_mean_var_ratio.device)
-            ) + torch.tensor(5.0, device=w_sf_mean_var_ratio.device)
+            w_sf_mean_var_ratio_hyp = pyro.sample(
+                "w_sf_mean_var_ratio_hyp_lik",
+                dist.Gamma(self.w_sf_mean_var_ratio_tensor, self.ones).expand([1, 1]).to_event(2),
+            )  # prior mean 5.0
+            w_sf_mean_var_ratio = pyro.sample(
+                "w_sf_mean_var_ratio_lik",
+                dist.Exponential(w_sf_mean_var_ratio_hyp).expand([1, self.n_factors]).to_event(2),
+            )  # (self.n_batch, self.n_vars) prior mean 0.2
+            if self.use_normal_likelihood:
+                w_sf_mean_var_ratio = w_sf_mean_var_ratio + torch.tensor(1.0 / 50.0, device=w_sf_mean_var_ratio.device)
+            else:
+                w_sf_mean_var_ratio = self.ones / (
+                    w_sf_mean_var_ratio + torch.tensor(1.0 / 50.0, device=w_sf_mean_var_ratio.device)
+                ) + torch.tensor(5.0, device=w_sf_mean_var_ratio.device)
         if tiles_unexpanded is not None:
             w_sf_mu_cell_comm = w_sf_mu_cell_comm[obs_in_use]
+            w_sf = w_sf[obs_in_use]
         if self.use_cell_abundance_normalisation:
             # =====================Location-specific detection efficiency ======================= #
             # y_s with hierarchical mean prior
@@ -382,9 +396,16 @@ class CellCommModule(PyroModule):
                     dist.Gamma(obs2sample @ detection_hyp_prior_alpha, beta),
                 )  # (self.n_obs, 1)
             w_sf_mu_cell_comm = w_sf_mu_cell_comm * detection_y_s
+
+        if w_sf_lvl2 is not None:
+            with obs_plate:
+                pyro.deterministic("w_sf_wo_lvl2_cell_comm", w_sf_mu_cell_comm)  # (self.n_obs, self.n_factors)
+            w_sf_mu_cell_comm = w_sf_mu_cell_comm * w_sf_lvl2
+
         with obs_plate:
             k = "w_sf"
             pyro.deterministic(f"{k}_cell_comm", w_sf_mu_cell_comm)  # (self.n_obs, self.n_factors)
+
         if self.use_alpha_likelihood:
             with obs_plate:
                 pyro.sample(

cell2location/nn/CellCommunicationToEffectNN.py
@@ -179,7 +179,7 @@ class CellCommunicationToTfActivityNN(
             layer=layer,
             weights_shape=weights_shape,
             bias_shape=[1],
-            random_init_scale=1 / np.sqrt(weights_shape[0]),
+            random_init_scale=1.0,
             bayesian=True,
             use_non_negative_weights=non_negative,
             weights_prior_tau=weights_prior_tau,
@@ -187,7 +187,7 @@ class CellCommunicationToTfActivityNN(
         )
         if upper_triangle:
             if len(weights.shape) > 2:
-                weights = torch.triu(torch.ones((weights.shape[0], weights[1]))).unsqueeze(-1) * weights
+                weights = torch.triu(torch.ones((weights.shape[0], weights.shape[1]))).unsqueeze(-1) * weights
             else:
                 weights = torch.triu(weights)
 
@@ -291,7 +291,7 @@ class CellCommunicationToTfActivityNN(
             layer=layer,
             weights_shape=weights_shape,
             bias_shape=[1],
-            random_init_scale=1 / np.sqrt(self.n_signals),
+            # random_init_scale=1 / np.sqrt(self.n_signals),
             bayesian=True,
             weights_prior_shape=torch.tensor(self.r_l_affinity_alpha_prior, device=x.device),
             weights_prior_rate=torch.tensor(self.r_l_affinity_alpha_prior, device=x.device),
@@ -324,10 +324,11 @@ class CellCommunicationToTfActivityNN(
             layer=layer,
             weights_shape=weights_shape,
             bias_shape=[1],
-            random_init_scale=1 / np.sqrt(len(self.signal_receptor_mask_scipy.data)),
+            random_init_scale=1.0,
             bayesian=True,
             # sample positive weights
             use_non_negative_weights=use_non_negative_weights,
+            use_horseshoe_prior=True,
         )
 
         if self.receptor_tf_mask is not None:
@@ -631,6 +632,13 @@ class CellCommunicationToTfActivityNN(
                 else [len(self.signal_receptor_mask_scipy.data), self.n_out],
                 name="signal_receptor_tf_effect",
             )
+        # normalise by sqrt of the number of predictors
+        tf_sig_rec_effect_hsr = tf_sig_rec_effect_hsr / torch.sqrt(
+            torch.tensor(
+                float(len(self.signal_receptor_mask_scipy.data)),
+                device=tf_sig_rec_effect_hsr.device,
+            )
+        )
 
         # print("tf_sig_rec_effect_hsr mean", tf_sig_rec_effect_hsr.mean())
         # print("tf_sig_rec_effect_hsr min", tf_sig_rec_effect_hsr.min())
@@ -731,6 +739,30 @@ class CellCommunicationToTfActivityNN(
 
         return effect_on_tf_abundance
 
+    def apply_upper_triangle_diag(self, weights, name, n_out=1):
+        if (len(weights.shape) == 3) and (n_out == 1):
+            shape = (weights.shape[1], weights.shape[2])
+            triu = torch.triu(torch.ones(shape)).unsqueeze(-1)
+            weights = weights * triu
+            zero_diag = torch.ones(shape, device=weights.device)
+            zero_diag = zero_diag.fill_diagonal_(0.0)
+            zero_diag = zero_diag.unsqueeze(-1)
+            weights = weights * zero_diag
+        elif (len(weights.shape) == 4) and (n_out > 1):
+            raise NotImplementedError
+        elif (len(weights.shape) == 3) and (n_out > 1):
+            raise NotImplementedError
+        elif (len(weights.shape) == 2) and (n_out == 1):
+            shape = (weights.shape[0], weights.shape[1])
+            triu = torch.triu(torch.ones(shape))
+            weights = weights * triu
+            zero_diag = torch.ones(shape, device=weights.device)
+            zero_diag = zero_diag.fill_diagonal_(0.0)
+            weights = weights * zero_diag
+        if not self.training:
+            pyro.deterministic(f"{name}_2_total_effect", weights)
+        return weights, zero_diag, triu
+
     def signal_receptor_pathway_tf_effect(
         self,
         bound_receptor_abundance_src: torch.Tensor,
@@ -760,11 +792,11 @@ class CellCommunicationToTfActivityNN(
             ],
             name="signal_receptor_pathway_effect",
         )
-        # normalised by sqrt of the number of predictors
+        # normalise by sqrt of the number of predictors
         pathway_sig_rec_effect_hsr = pathway_sig_rec_effect_hsr / torch.sqrt(
             torch.tensor(
                 float(len(self.signal_receptor_mask_scipy.data)),
-                device=bound_receptor_abundance_src.device,
+                device=pathway_sig_rec_effect_hsr.device,
             )
         )
 
@@ -818,6 +850,7 @@ class CellCommunicationToTfActivityNN(
                 else [self.n_tfs, self.n_pathways * self.n_out],
                 remove_diagonal=False,
                 non_negative=self.use_non_negative_weights,
+                use_horseshoe_prior=True,
             )
         else:
             pathway_tf_weights = self.get_tf_effect(
@@ -827,7 +860,15 @@ class CellCommunicationToTfActivityNN(
                 weights_shape=[self.n_pathways] if self.n_out == 1 else [self.n_pathways * self.n_out],
                 remove_diagonal=False,
                 non_negative=self.use_non_negative_weights,
+                use_horseshoe_prior=True,
             )
+        # normalise by sqrt of the number of predictors
+        pathway_tf_weights = pathway_tf_weights / torch.sqrt(
+            torch.tensor(
+                float(self.n_pathways),
+                device=pathway_tf_weights.device,
+            )
+        )
         if use_cell_abundance_model:
             effect_on_pathway_activity = rearrange(
                 effect_on_pathway_activity,
@@ -867,6 +908,7 @@ class CellCommunicationToTfActivityNN(
                     else [self.n_tfs, self.n_pathways * self.n_pathways * self.n_out],
                     remove_diagonal=False,
                     non_negative=self.use_non_negative_weights,
+                    use_horseshoe_prior=True,
                 )
             else:
                 pathway_tf_weights = self.get_tf_effect(
@@ -878,6 +920,7 @@ class CellCommunicationToTfActivityNN(
                     else [self.n_pathways * self.n_pathways * self.n_out],
                     remove_diagonal=False,
                     non_negative=self.use_non_negative_weights,
+                    use_horseshoe_prior=True,
                 )
             if self.n_out == 1:
                 if self.use_global_cell_abundance_model:
@@ -887,6 +930,7 @@ class CellCommunicationToTfActivityNN(
                         p=self.n_pathways,
                         o=self.n_pathways,
                     )
+                    pathway_tf_weights, zero_diag, triu = self.apply_upper_triangle_diag(pathway_tf_weights, name)
                 else:
                     pathway_tf_weights = rearrange(
                         pathway_tf_weights,
@@ -894,6 +938,9 @@ class CellCommunicationToTfActivityNN(
                         p=self.n_pathways,
                         o=self.n_pathways,
                     )
+                    pathway_tf_weights, zero_diag, triu = self.apply_upper_triangle_diag(pathway_tf_weights, name)
+                # normalise by sqrt of the number of predictors
+                pathway_tf_weights = pathway_tf_weights / torch.sqrt((zero_diag * triu).sum())
                 if use_cell_abundance_model:
                     if self.use_global_cell_abundance_model:
                         effect_on_tf_activity = effect_on_tf_activity + torch.einsum(
@@ -925,6 +972,11 @@ class CellCommunicationToTfActivityNN(
                         o=self.n_pathways,
                         f=self.n_out,
                     )
+                    pathway_tf_weights, zero_diag, triu = self.apply_upper_triangle_diag(
+                        pathway_tf_weights,
+                        name,
+                        n_out=self.n_out,
+                    )
                 else:
                     pathway_tf_weights = rearrange(
                         pathway_tf_weights,
@@ -933,6 +985,13 @@ class CellCommunicationToTfActivityNN(
                         o=self.n_pathways,
                         f=self.n_out,
                     )
+                    pathway_tf_weights, zero_diag, triu = self.apply_upper_triangle_diag(
+                        pathway_tf_weights,
+                        name,
+                        n_out=self.n_out,
+                    )
+                # normalise by sqrt of the number of predictors
+                pathway_tf_weights = pathway_tf_weights / torch.sqrt((zero_diag * triu).sum())
                 if use_cell_abundance_model:
                     if self.use_global_cell_abundance_model:
                         effect_on_tf_activity = torch.einsum(